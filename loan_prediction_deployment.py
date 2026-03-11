cb1492a671cc5a37f8c9283d6b1c5d90dba2ab5a
darelie-2122 <136192545+darelie-2122@users.noreply.github.com>
2026-03-11 22:43:25 +0530

    Update loan_prediction_deployment.py

--- loan_prediction_deployment.py
+++ loan_prediction_deployment.py
@@ -11,39 +11,42 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("Loan_prediction_rfr_model.pkl")
-encoder = joblib.load("label_encoder_Loan.pkl")
+# load model
+model = joblib.load("loan_prediction_lr_model.pkl")
+encoder = joblib.load("label_encoder_Loan_prediction.pk1")
+
+st.title("Loan Approval Prediction")
+
+st.write("Enter applicant details to predict loan approval")
+
+Gender = st.selectbox("Gender", ["Male","Female"])
+Married = st.selectbox("Married", ["Yes","No"])
+Dependents = st.selectbox("Dependents", ["0","1","2","3+"])
+Education = st.selectbox("Education", ["Graduate","Not Graduate"])
+Self_Employed = st.selectbox("Self Employed", ["Yes","No"])
+ApplicantIncome = st.number_input("Applicant Income", min_value=1000)
+CoapplicantIncome = st.number_input("Coapplicant Income",0)
+LoanAmount = st.number_input("Loan Amount", min_value=50)
+Loan_Amount_Term = st.number_input("Loan Amount Term", min_value=12)
+Credit_History = st.selectbox("Credit History", [0,1])
+Property_Area = st.selectbox("Property Area", ["Urban","Semiurban","Rural"])
 
 
-st.title("Loan Status prediction model")
-
-loan_id = st.text_input("Enter Loan ID")
-gender = st.selectbox("Select Gender",encoder["Gender"].classes_)
-
-married = st.selectbox("Marital Status", encoder["Married"].classes_)
-dependents = st.selectbox("Number of Dependents",encoder["Dependents"].classes_)
-education = st.selectbox("Education",encoder["Education"].classes_)
-self_employed = st.selectbox("Self Employed",encoder["Self_Employed"].classes_)
-applicant_income = st.number_input("Applicant Income",min_value=0)
-coapplicant_income = st.number_input("Coapplicant Income",min_value=0)
-loan_amount = st.number_input("Loan Amount",min_value=0)
-loan_term = st.number_input("Loan Amount Term (months)", min_value=0)
-credit_history = st.selectbox("Credit History", [0, 1])
-property_area = st.selectbox("Property Area",encoder["Property_Area"].classes_)
-
+# convert input to dataframe
 df = pd.DataFrame({
-    "Gender": [gender],
-    "Married": [married],
-    "Dependents": [dependents],
-    "Education": [education],
-    "Self_Employed": [self_employed],
-    "ApplicantIncome": [applicant_income],
-    "CoapplicantIncome": [coapplicant_income],
-    "LoanAmount": [loan_amount],
-    "Loan_Amount_Term": [loan_term],
-    "Credit_History": [credit_history],
-    "Property_Area": [property_area]
+    "Gender":[Gender],
+    "Married":[Married],
+    "Dependents":[Dependents],
+    "Education":[Education],
+    "Self_Employed":[Self_Employed],
+    "ApplicantIncome":[ApplicantIncome],
+    "CoapplicantIncome":[CoapplicantIncome],
+    "LoanAmount":[LoanAmount],
+    "Loan_Amount_Term":[Loan_Amount_Term],
+    "Credit_History":[Credit_History],
+    "Property_Area":[Property_Area]
 })
+
 
 if st.button("Predict Loan Status"):
 
@@ -53,4 +56,7 @@ if st.button("Predict Loan Status"):
 
     prediction = model.predict(df)
 
-    st.success(f"Predicted Loan Status: {prediction[0]}")
+    if prediction[0] >= 0.5:
+        st.success("Loan Approved")
+    else:
+        st.error("Loan Not Approved")
